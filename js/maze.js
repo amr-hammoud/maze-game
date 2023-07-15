9538235ba8b4e96e01313ebc369d110c3881972b
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-15 14:30:30 +0300

    added game scene for testing

---
+++ js/maze.js
@@ -0,0 +1,36 @@
+class Scene1 extends Phaser.Scene
+{
+    constructor ()
+    {
+        super('homePage')
+    }
+
+    preload ()
+    {
+        this.load.image('bg_lvl1', 'assets/game_images/level_backgrouds/Blue.png')
+    }
+      
+    create ()
+    {
+        this.background = this.add.image(0,0,'bg_lvl1' )
+        this.background.setOrigin(0,0)
+    }
+    update(){
+
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 500,
+    scene: [Scene1,],
+    physics: {
+        default: "arcade",
+        arcade: {
+          debug: false
+        }
+      }
+};
+
+let game = new Phaser.Game(config)
