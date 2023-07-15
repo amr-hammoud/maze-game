45671530c1329c74483847640f65090c37410375
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-15 18:59:03 +0300

    added transition to scene 3(level2)

--- js/maze.js
+++ js/maze.js
@@ -8,6 +8,7 @@ class Scene1 extends Phaser.Scene
     preload ()
     {
         this.load.image('bg_lvl1', 'assets/game_images/level_backgrouds/Blue.png')
+        this.load.image('bg_lvl2', 'assets/game_images/level_backgrouds/Green.png')
         this.load.image('start_btn', 'assets/game_images/buttons/button-start.png')
         this.load.spritesheet('ninja', 'assets/game_images/character/Run (32x32).png', {frameWidth: 32, frameHeight:32})
         this.load.spritesheet('trap', 'assets/game_images/traps/trap(38x38).png', {frameWidth: 38, frameHeight:38})
@@ -65,7 +66,7 @@ class Scene1 extends Phaser.Scene
 }
 
 
-class Scene2 extends Phaser.Scene
+class Scene2Level1 extends Phaser.Scene
 {
     constructor ()
     {
@@ -76,16 +77,17 @@ class Scene2 extends Phaser.Scene
         this.load.image('wallh_10', 'assets/game_images/walls/h10.png')
         this.load.image('wallv_6', 'assets/game_images/walls/v6.png')
         this.load.image('big_wall', 'assets/game_images/walls/Terrain (16x16).png')
+        this.load.image('to_lvl_2', 'assets/game_images/level_ending/02.png')
     }
     create(){
         this.background = this.add.tileSprite(200,0,600, 500, 'bg_lvl1' )
         this.background.setOrigin(0,0)
         this.cursorKeys = this.input.keyboard.createCursorKeys()
 
-        this.wall_border_down = this.physics.add.image(525,450,'wallh_10')
-        this.wall_border_up = this.physics.add.image(525,50,'wallh_10')
-        this.wall_border_righ = this.physics.add.image(755,300,'wallv_6')
-        this.wall_border_left = this.physics.add.image(294,203,'wallv_6')
+        // this.wall_border_down = this.physics.add.image(525,450,'wallh_10')
+        // this.wall_border_up = this.physics.add.image(525,50,'wallh_10')
+        // this.wall_border_righ = this.physics.add.image(755,300,'wallv_6')
+        // this.wall_border_left = this.physics.add.image(294,203,'wallv_6')
         this.big_wall1 = this.physics.add.image(243,203,'big_wall')
         this.big_wall1.scale = 1.35
         this.big_wall2 = this.physics.add.image(500,480,'big_wall')
@@ -94,15 +96,15 @@ class Scene2 extends Phaser.Scene
 
 
         this.walls_grp = this.physics.add.group()
-        this.walls_grp.add(this.wall_border_down)
-        this.walls_grp.add(this.wall_border_left)
-        this.walls_grp.add(this.wall_border_up)
-        this.walls_grp.add(this.wall_border_righ)
+        this.walls_grp.add(this.physics.add.image(525,450,'wallh_10'))
+        this.walls_grp.add(this.physics.add.image(525,50,'wallh_10'))
+        this.walls_grp.add(this.physics.add.image(755,300,'wallv_6'))
+        this.walls_grp.add(this.physics.add.image(294,203,'wallv_6'))
         this.walls_grp.add(this.big_wall1)
         this.walls_grp.add(this.big_wall2)
         
 
-        this.ninja = this.physics.add.sprite(325,450,'ninja')
+        this.ninja = this.physics.add.sprite(225,450,'ninja')
         this.character_grp = this.physics.add.group()
         this.character_grp.add(this.ninja)
         this.ninja.setCollideWorldBounds(true)
@@ -110,6 +112,11 @@ class Scene2 extends Phaser.Scene
 
         this.ninja.play('ninja_anim')
         this.physics.world.setBounds(200, 0, 800, 500);
+
+        this.to_lvl_2 = this.physics.add.sprite(780,100,'to_lvl_2')
+        this.to_lvl_2.scale = 2
+
+        this.physics.add.collider(this.ninja,this.to_lvl_2, () => this.scene.start('gameLevelTwo'))
 
     }
     update(){
@@ -149,11 +156,32 @@ class Scene2 extends Phaser.Scene
 
 }
 
+class Scene3Level2 extends Phaser.Scene
+{
+    constructor ()
+    {
+        super('gameLevelTwo');
+    }
+    preload ()
+    {
+
+
+    }
+    create(){
+        this.background = this.add.tileSprite(200,0,600, 500, 'bg_lvl2' )
+        this.background.setOrigin(0,0)
+    }
+    update(){
+    }
+    
+
+}
+
 const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 500,
-    scene: [Scene1, Scene2],
+    scene: [Scene1, Scene2Level1, Scene3Level2],
     physics: {
         default: "arcade",
         arcade: {
