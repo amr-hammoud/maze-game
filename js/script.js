6239e3e30b14d242bb8b0a13a3cbca9696b2cb5b
mohamadyoussef24 <mohammadyoussef.cs@gmail.com>
2023-07-15 20:05:08 +0300

    add bushes parallax

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-//text mouve
-gsap.fromTo(".txt",{y:500},{y:100 , duration:5 , scale:3});
+gsap.fromTo(".txt",{y:500},{y:100 , duration:5 , scale:3}); //parallax for text
 
-gsap.fromTo(".birds",{y:0},{y:150 , duration:5 , scale:1});
+gsap.fromTo(".birds",{y:0},{y:150 , duration:5 , scale:1}); //animation for birds
 
+//cloud parallax scroling
 gsap.from(".cloud1",{
     scrollTrigger : {
         scrub: true
@@ -17,23 +17,6 @@ gsap.from(".cloud2",{
     },
     x: 600,
 })
-gsap.from(".bushes",{
-    scrollTrigger : {
-        scrub: true
-
-    },
-    x: -150,
-    scale:1,
-})
-gsap.from(".button",{
-    scrollTrigger : {
-        scrub: true
-
-    },
-    y:400,
-    scale:2,
-})
-
 
 
 
