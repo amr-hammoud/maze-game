1945ee57a602bd8895b29f4d3778ec2d52a95cbe
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-15 18:37:48 +0300

    added ninja and trap animations to main menu

--- js/maze.js
+++ js/maze.js
@@ -9,6 +9,9 @@ class Scene1 extends Phaser.Scene
     {
         this.load.image('bg_lvl1', 'assets/game_images/level_backgrouds/Blue.png')
         this.load.image('start_btn', 'assets/game_images/buttons/button-start.png')
+        this.load.spritesheet('ninja', 'assets/game_images/character/Run (32x32).png', {frameWidth: 32, frameHeight:32})
+        this.load.spritesheet('trap', 'assets/game_images/traps/trap(38x38).png', {frameWidth: 38, frameHeight:38})
+
     }
       
     create ()
@@ -17,6 +20,44 @@ class Scene1 extends Phaser.Scene
         this.background.setOrigin(0,0)
         this.start_btn = this.add.image(100,400, 'start_btn')
         this.start_btn.setInteractive({ useHandCursor: true }).on('pointerdown', () => this.scene.start('gameLevelOne'))
+
+        this.ninja = this.add.sprite(275,450,'ninja')
+        this.anims.create({
+            key:'ninja_anim',
+            frames: this.anims.generateFrameNumbers('ninja'),
+            frameRate:20,
+            repeat: -1
+        })
+        this.ninja.play('ninja_anim')
+
+        this.trap = this.add.sprite(225,450,'trap')
+        this.anims.create({
+            key:'trap_anim',
+            frames: this.anims.generateFrameNumbers('trap'),
+            frameRate:20,
+            repeat: -1
+        })
+        this.trap.play('trap_anim')
+
+        this.tweens.add({
+            targets: this.ninja,
+            x: 750,
+            y: 450,
+            duration: 2000,
+            ease: "Power2",
+            yoyo: true,
+            loop: -1
+        });
+
+        this.tweens.add({
+            targets: this.trap,
+            x: 700,
+            y: 450,
+            duration: 2000,
+            ease: "Power2",
+            yoyo: true,
+            loop: -1
+        });
     }
     update(){
 
@@ -32,7 +73,6 @@ class Scene2 extends Phaser.Scene
     }
     preload ()
     {
-        this.load.spritesheet('ninja', 'assets/game_images/character/Run (32x32).png', {frameWidth: 32, frameHeight:32})
         this.load.image('wallh_10', 'assets/game_images/walls/h10.png')
         this.load.image('wallv_6', 'assets/game_images/walls/v6.png')
         this.load.image('big_wall', 'assets/game_images/walls/Terrain (16x16).png')
@@ -51,6 +91,8 @@ class Scene2 extends Phaser.Scene
         this.big_wall2 = this.physics.add.image(500,480,'big_wall')
         this.big_wall2.scale = 0.85
 
+
+
         this.walls_grp = this.physics.add.group()
         this.walls_grp.add(this.wall_border_down)
         this.walls_grp.add(this.wall_border_left)
@@ -60,18 +102,12 @@ class Scene2 extends Phaser.Scene
         this.walls_grp.add(this.big_wall2)
         
 
-        this.ninja = this.physics.add.sprite(225,450,'ninja')
-        this.ninja.enableBody = true
+        this.ninja = this.physics.add.sprite(325,450,'ninja')
         this.character_grp = this.physics.add.group()
         this.character_grp.add(this.ninja)
         this.ninja.setCollideWorldBounds(true)
         this.ninja.scale = 1.4
-        this.anims.create({
-            key:'ninja_anim',
-            frames: this.anims.generateFrameNumbers('ninja'),
-            frameRate:20,
-            repeat: -1
-        })
+
         this.ninja.play('ninja_anim')
         this.physics.world.setBounds(200, 0, 800, 500);
 
