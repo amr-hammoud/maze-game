d54bb21c381997b658187e63216753ff691dcf6c
mohamadyoussef24 <mohammadyoussef.cs@gmail.com>
2023-07-15 15:01:43 +0300

    adding parallax to text

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//text mouve
+gsap.from(".txt",{
+    scrollTrigger : {
+        scrub: true
+
+    },
+    y: 500,
+})
+//brids move
+gsap.from(".birds",{
+    scrollTrigger : {
+        scrub: true
+
+    },
+    y: 40,
+})
+gsap.from(".birds",{
+    scrollTrigger : {
+        scrub: true
+
+    },
+    y: -60,
+})
+gsap.from(".birds",{
+    scrollTrigger : {
+        scrub: true
+
+    },
+    y: 30,
+})
+
