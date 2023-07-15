b4fca6cc2ed4d353f1be4084182b94fd6afeb4af
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-15 15:53:56 +0300

    added game assets and test scene2

--- js/maze.js
+++ js/maze.js
@@ -8,23 +8,49 @@ class Scene1 extends Phaser.Scene
     preload ()
     {
         this.load.image('bg_lvl1', 'assets/game_images/level_backgrouds/Blue.png')
+        this.load.image('start_btn', 'assets/game_images/buttons/button-start.png')
     }
       
     create ()
     {
         this.background = this.add.tileSprite(200,0,600, 500, 'bg_lvl1' )
         this.background.setOrigin(0,0)
+        this.start_btn = this.add.image(100,400, 'start_btn')
+        this.start_btn.setInteractive({ useHandCursor: true }).on('pointerdown', () => this.scene.start('gameLevelOne'))
     }
     update(){
 
     }
 }
 
+
+class Scene2 extends Phaser.Scene
+{
+    constructor ()
+    {
+        super('gameLevelOne');
+    }
+    preload ()
+    {
+
+
+    }
+    create(){
+        this.background = this.add.tileSprite(200,0,600, 500, 'bg_lvl1' )
+        this.background.setOrigin(0,0)
+    }
+    update(){
+        
+    }
+    
+
+}
+
 const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 500,
-    scene: [Scene1,],
+    scene: [Scene1, Scene2],
     physics: {
         default: "arcade",
         arcade: {
